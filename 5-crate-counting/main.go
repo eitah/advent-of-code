fbde4fcc55cd36d0187b3be11fe9f48c054f3a9a
Eli Itah <eli@spantree.net>
2022-12-05 21:32:57 -0600

    Refactor to only use newscanner once

--- 5-crate-counting/main.go
+++ 5-crate-counting/main.go
@@ -15,7 +15,10 @@ func main() {
 	}
 }
 
+// a crate stack is an array of letters made to represent the contents of crates.
 type CrateStack []rune
+
+// A Yard represents an array of 1 or more crate stacks.
 type Yard []CrateStack
 
 type MoveCommand struct {
@@ -24,31 +27,45 @@ type MoveCommand struct {
 	end       int
 }
 
+// this + in the regex took me a long time fo figure out.
+var moveLineParseRegex = regexp.MustCompile("^move (\\d+) from (\\d+) to (\\d+)")
+var isAContainerRowRegex = regexp.MustCompile("\\[")
+
 func mainErr() error {
 	readFile, err := os.Open("crate-counting.txt")
 	if err != nil {
 		return err
 	}
-	defer readFile.Close()
 
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
 
 	// the make syntax is nice here because I don't have to worry about an array not being declared when assigning to this
 	containers := make(Yard, 9)
-	for fileScanner.Scan() {
-		containers = parseContainers(containers, fileScanner.Text())
-	}
 
 	for fileScanner.Scan() {
-		cmd, err := moveLineToCommand(fileScanner.Text())
-		if err != nil {
-			// swallow errors if unable to parse move lines
+		if isAContainerRowRegex.FindString(fileScanner.Text()) != "" {
+			// if it is the container map, parse it
+			containers = parseContainers(containers, fileScanner.Text())
+		}
+
+		result := moveLineParseRegex.FindAllStringSubmatch(fileScanner.Text(), -1)
+		if result == nil {
 			continue
 		}
 
-		containers = moveAllCratesAtOnce(containers, cmd)
+		if len(result[0]) != 4 {
+			return fmt.Errorf("Malformed input at %s", fileScanner.Text())
+		}
 
+		cmd := &MoveCommand{
+			numCrates: unsafeStrToNum(result[0][1]),
+			start:     unsafeStrToNum(result[0][2]),
+			end:       unsafeStrToNum(result[0][3]),
+		}
+
+		// containers = moveOneCrateAtATime(containers, cmd)
+		containers = moveAllCratesAtOnce(containers, cmd)
 	}
 	printWordFromContainers(containers)
 
@@ -56,7 +73,11 @@ func mainErr() error {
 }
 
 func printWordFromContainers(y Yard) {
+
 	for _, stack := range y {
+		if len(stack) == 0 {
+			continue
+		}
 		fmt.Print(string(stack[len(stack)-1]))
 	}
 
@@ -68,6 +89,7 @@ func moveAllCratesAtOnce(containers Yard, cmd *MoveCommand) Yard {
 	z := containers[cmd.end-1]
 	var boxesToMove CrateStack
 	if cmd.numCrates == len(a) {
+		// this prevents indexing errors but oof is it weird.
 		boxesToMove = a
 		a = CrateStack{}
 	} else {
@@ -83,7 +105,8 @@ func moveAllCratesAtOnce(containers Yard, cmd *MoveCommand) Yard {
 
 func moveOneCrateAtATime(containers Yard, cmd *MoveCommand) Yard {
 	for i := 1; i <= cmd.numCrates; i++ {
-		fmt.Println(cmd.numCrates, i)
+
+		// fmt.Println(cmd.numCrates, i)
 		a := containers[cmd.start-1]
 		topBox, a := a[len(a)-1], a[:len(a)-1]
 		z := containers[cmd.end-1]
@@ -97,19 +120,21 @@ func moveOneCrateAtATime(containers Yard, cmd *MoveCommand) Yard {
 	return containers
 }
 
-var moveLineParseRegex = regexp.MustCompile("^move (\\d+) from (\\d) to (\\d)")
-
-func moveLineToCommand(moveLine string) (*MoveCommand, error) {
-	result := moveLineParseRegex.FindAllStringSubmatch(moveLine, -1)
-	if result == nil {
-		return nil, fmt.Errorf("not a move command")
+func parseContainers(yard Yard, text string) Yard {
+	for index, rn := range text {
+		if unicode.IsLetter(rn) {
+			// index in yard-1 is 0, which maps to the 0th column in the yard, and every 4th row thereafter
+			indexInYard := (index - 1) / 4
+			// containers are added to the stack in reverse order to how they are printed in the file, this prepends them
+			yard[indexInYard] = unshiftRune(yard[indexInYard], rn)
+		}
 	}
 
-	return &MoveCommand{
-		numCrates: unsafeStrToNum(result[0][1]),
-		start:     unsafeStrToNum(result[0][2]),
-		end:       unsafeStrToNum(result[0][3]),
-	}, nil
+	return yard
+}
+
+func unshiftRune(stack CrateStack, rn rune) CrateStack {
+	return append(CrateStack{rn}, stack...)
 }
 
 func unsafeStrToNum(str string) int {
@@ -119,25 +144,6 @@ func unsafeStrToNum(str string) int {
 		panic(err)
 	}
 	return num
-}
-
-var isAContainerRowRx = regexp.MustCompile("\\[")
-
-func parseContainers(yard Yard, text string) Yard {
-	if isAContainerRowRx.FindString(text) == "" {
-		return yard
-	}
-	fmt.Println(text)
-	for index, rn := range text {
-		if unicode.IsLetter(rn) {
-			indexInYard := (index - 1) / 4
-			// containers are added to the stack in reverse order to how they are printed in the file, this unshifts em
-			yard[indexInYard] = append(CrateStack{rn}, yard[indexInYard]...)
-			fmt.Println(string(rn), index-1)
-		}
-	}
-
-	return yard
 }
 
 // this is what the hardcoded list of crates looked like before I parsed em
