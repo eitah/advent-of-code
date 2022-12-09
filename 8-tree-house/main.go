67cca64d87f461255d4893c0e977f0adb8f653c2
Eli Itah <eli@spantree.net>
2022-12-08 19:02:28 -0600

    End of part 2

--- 8-tree-house/main.go
+++ 8-tree-house/main.go
@@ -14,8 +14,10 @@ func main() {
 }
 
 type Tree struct {
-	height  int
-	visible bool
+	height      int
+	visible     bool
+	rowscore    int
+	columnscore int
 }
 type Grid [][]*Tree
 
@@ -49,13 +51,19 @@ func mainErr() error {
 		rowIndex++
 	}
 
-	treegrid = checkVisibility(treegrid)
+	treegrid = checkVisibility1(treegrid)
+	treegrid = checkVisibility2(treegrid)
 
-	// fmt.Printf("%s\n", treegrid)
-	// spew.Dump(treegrid)
+	// round 1
+	scoreRound1(treegrid)
+	scoreRound2(treegrid)
 
+	return nil
+}
+
+func scoreRound1(grid Grid) {
 	var visible, invisible int
-	for _, row := range treegrid {
+	for _, row := range grid {
 		for _, tree := range row {
 			if tree.visible {
 				visible++
@@ -66,12 +74,23 @@ func mainErr() error {
 	}
 
 	fmt.Printf("Result: %d visible, %d invisible\n", visible, invisible)
-
-	return nil
 }
 
-// todo address https://stackoverflow.com/questions/23330781/collect-values-in-order-each-containing-a-map
-func checkVisibility(grid Grid) Grid {
+func scoreRound2(grid Grid) {
+	var highest int
+	for _, row := range grid {
+		for _, tree := range row {
+			score := tree.rowscore * tree.columnscore
+			if score > highest {
+				highest = score
+			}
+		}
+	}
+
+	fmt.Println("highest scenic score is", highest)
+}
+
+func checkVisibility1(grid Grid) Grid {
 	for idx, row := range grid {
 		row = CheckRow(row)
 		grid[idx] = row
@@ -126,6 +145,75 @@ func CheckRow(row []*Tree) []*Tree {
 		if tree.height > tallestTreeSeen {
 			tree.visible = true
 			tallestTreeSeen = tree.height
+		}
+	}
+
+	return row
+}
+
+func checkVisibility2(grid Grid) Grid {
+	for idx, row := range grid {
+		row = checkViewRow(row, false)
+		grid[idx] = row
+	}
+
+	for i := 0; i < len(grid[0]); i++ {
+		// for i := 2; i < 3; i++ {
+		var column []*Tree
+		for _, row := range grid {
+			column = append(column, row[i])
+		}
+
+		column = checkViewRow(column, true)
+	}
+
+	return grid
+}
+
+func checkViewRow(row []*Tree, isAColumn bool) []*Tree {
+	for idx, tree := range row {
+		if idx == 0 || idx == len(row)-1 {
+			// edge trees always have score 0
+			if isAColumn {
+				tree.columnscore = 0
+			} else {
+				tree.rowscore = 0
+			}
+			continue
+		}
+
+		var rightscore, leftscore int
+		// right:
+		var stop bool
+		for i := idx + 1; i < len(row); i++ {
+			if !stop {
+				if row[i].height < tree.height {
+					rightscore++
+				} else {
+					stop = true
+					rightscore++ // count the last tree seen
+				}
+			}
+
+		}
+
+		// left:
+		stop = false
+		for i := idx - 1; i >= 0; i-- {
+			if !stop {
+				if row[i].height < tree.height {
+					leftscore++
+				} else {
+					stop = true
+					leftscore++ // count the last tree seen
+				}
+			}
+		}
+
+		if isAColumn {
+			tree.columnscore = rightscore * leftscore
+		} else {
+			tree.rowscore = rightscore * leftscore
 		}
 	}
 
