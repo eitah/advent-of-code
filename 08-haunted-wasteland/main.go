4ca1616b01717978c0a253230252f482d325e381
Eli Itah <eli.itah@tempus.com>
2023-12-08 09:34:49 -0600

    Day 8 part 2 after accepting that I need LCM instead of brute force

--- 08-haunted-wasteland/main.go
+++ 08-haunted-wasteland/main.go
@@ -15,7 +15,8 @@ func main() {
 	}
 }
 
-type Elf []int64
+var lefts = make(map[string]string)
+var rights = make(map[string]string)
 
 func mainErr() error {
 	readFile, err := os.Open("short.txt")
@@ -43,8 +44,7 @@ func mainErr() error {
 	}
 
 	var turns []string
-	lefts := make(map[string]string)
-	rights := make(map[string]string)
+
 	for idx, line := range input {
 		if idx == 0 {
 			parts := strings.Split(line, "")
@@ -62,10 +62,121 @@ func mainErr() error {
 		}
 	}
 
+	// part 1
+	// var count int
+	// destination := "AAA"
+	// // for count < 10 {
+	// for destination != "ZZZ" {
+	// 	for i := 0; i < len(turns); i++ {
+	// 		turn := turns[i]
+	// 		if turn == "L" {
+	// 			destination = lefts[destination]
+	// 		} else {
+	// 			destination = rights[destination]
+	// 		}
+
+	// 		count++
+
+	// 		spew.Dump(destination)
+	// 		if destination == "ZZZ" {
+	// 			fmt.Printf("we did it!, %d\n", count)
+	// 			break
+	// 		}
+	// 	}
+	// }
+
+	// part 2
+	// compute starting spaces, lefts and rights are identical
+	var ghostDestination []string
+	for dest := range lefts {
+		if string(dest[2]) == "A" {
+			ghostDestination = append(ghostDestination, dest)
+		}
+	}
+
+	out := make(map[string]int, len(ghostDestination))
+	for _, dest := range ghostDestination {
+		out[dest] = findSolutionForOne(turns, dest)
+	}
+	spew.Dump(out)
+
+	var vals []int
+	for _, turns := range out {
+		vals = append(vals, turns)
+	}
+
+	spew.Dump(LCM(vals[0], vals[1], vals[2:]...))
+	return nil
+}
+
+// Least common eucledean algorithm
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
+
+// GCD is greatest common divisor euclidian algorthm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// brute force implementation
+// var count int
+// for count < 2 {
+// for !didIWin(ghostDestination) {
+// 	for i := 0; i < len(turns); i++ {
+// 		// copy ghost destinations
+// 		beforeTurning := ghostDestination
+// 		// clear out ghost destinations now that its copied
+// 		ghostDestination = []string{}
+// 		turn := turns[i]
+// 		if turn == "L" {
+// 			for _, dest := range beforeTurning {
+// 				ghostDestination = append(ghostDestination, lefts[dest])
+// 			}
+// 		} else {
+// 			for _, dest := range beforeTurning {
+// 				// fmt.Println(dest, rights[dest])
+// 				ghostDestination = append(ghostDestination, rights[dest])
+// 			}
+// 			// fmt.Println()
+// 		}
+
+// 		count++
+
+// 		// if count > 10 {
+// 		// 	break
+// 		// }
+// 		// fmt.Println(count, turn, ghostDestination)
+// 		// fmt.Println(ghostDestination)
+
+// 		if didIWin(ghostDestination) {
+// 			fmt.Printf("we did it! %d, %s\n", count, ghostDestination)
+// 			break
+// 		}
+
+// 		if didILose(ghostDestination) {
+// 			fmt.Printf("we Lost! %d %s\n", count, ghostDestination)
+// 			break
+// 		}
+// 	}
+// }
+
+func findSolutionForOne(turns []string, initial string) int {
 	var count int
-	destination := "AAA"
+	destination := initial
 	// for count < 10 {
-	for destination != "ZZZ" {
+	for !didIWin([]string{destination}) {
 		for i := 0; i < len(turns); i++ {
 			turn := turns[i]
 			if turn == "L" {
@@ -77,11 +188,30 @@ func mainErr() error {
 			count++
 
 			spew.Dump(destination)
-			if destination == "ZZZ" {
+			if didIWin([]string{destination}) {
 				fmt.Printf("we did it!, %d\n", count)
 				break
 			}
 		}
 	}
-	return nil
+
+	return count
+}
+
+func didIWin(dests []string) bool {
+	for _, dest := range dests {
+		if string(dest[2]) != "Z" {
+			return false
+		}
+	}
+	return true
+}
+
+func didILose(dests []string) bool {
+	for _, dest := range dests {
+		if string(dest[2]) != "A" {
+			return false
+		}
+	}
+	return true
 }
