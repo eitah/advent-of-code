071e60ef34bbfa5e6663e646c56155278709f712
Eli Itah <eli.itah@tempus.com>
2023-12-15 22:55:19 -0600

    Not working statae for puzzle day 2 but have to move on because this sucks

--- 13-point-of-incidence/main.go
+++ 13-point-of-incidence/main.go
@@ -54,7 +54,7 @@ func main() {
 		if result := checkForReflection(puzzle); result != -1 {
 			// fmt.Println("idx" + string(idx) + "puzzle reflects at" + string(result))
 			reflectedCols = append(reflectedCols, result)
-			fmt.Println("puzzle", idx, "reflects at", result)
+			fmt.Println("puzzle", idx, "reflects cols at", result)
 
 		}
 	}
@@ -65,7 +65,7 @@ func main() {
 			// fmt.Println("idx" + fmt.Sprint(idx) + "puzzle reflects at" + fmt.Sprint(result))
 			reflectedRows = append(reflectedRows, result)
 
-			fmt.Println("puzzle", idx, "reflects at", result)
+			fmt.Println("puzzle", idx, "reflects rows at", result)
 		}
 	}
 
@@ -82,34 +82,125 @@ func main() {
 	}
 
 	fmt.Println(sum)
+}
 
+type Mirror struct {
+	Self     int
+	Top      int
+	Bottom   int
+	WishUsed bool
+	Puzzle   []string
+	IsValid  bool
 }
 
 func checkForReflection(puzzle []string) int {
-	var possibleMirrorRows []int
+	var possibleMirrorRows []Mirror
 
 	// for every word in the puzzle check if it matches the n-1 word
 	for i := 1; i < len(puzzle); i++ {
 		// fmt.Println(i)
-		if puzzle[i] == puzzle[i-1] {
-			possibleMirrorRows = append(possibleMirrorRows, i) // guess at if theres a mirror
+		if almostIdentical(puzzle[i-1], puzzle[i]) {
+			possibleMirrorRows = append(possibleMirrorRows, Mirror{
+				Self:     i,
+				Top:      i,
+				Bottom:   i - 1,
+				WishUsed: false,
+				Puzzle:   puzzle,
+				IsValid:  true, // all mirrors
+			})
 		}
 	}
 
-	// i couldnt get away with not checking em all
-	// if len(possibleMirrorRows) > 1 {
-	// 	panic("my algo lazily didnt account for 2 mirror possiblities but it could have\n" + strings.Join(puzzle, "\n") + fmt.Sprintf("%v", possibleMirrorRows))
-	// }
-
-	for _, rowId := range possibleMirrorRows {
-		if recursiveReflectionCheck(puzzle, rowId-1, rowId) {
-			return rowId
+	for _, mirror := range possibleMirrorRows {
+		mirror = gradeMirror(mirror)
+		if mirror.IsValid && mirror.WishUsed {
+			return mirror.Self
 		}
-
 	}
 
 	return -1
 }
+
+// grade mirror takes a mirror and decides if it should set wish used and invalid
+func gradeMirror(mirror Mirror) Mirror {
+	if !mirror.IsValid {
+		return mirror
+	}
+
+	if mirror.Bottom == 0 {
+		return isMirrorValid(mirror)
+	}
+
+	if mirror.Top == len(mirror.Puzzle)-1 {
+		return isMirrorValid(mirror)
+	}
+
+	mirror = isMirrorValid(mirror)
+
+	if mirror.IsValid {
+		mirror.Top += 1
+		mirror.Bottom -= 1
+		return gradeMirror(mirror)
+	}
+
+	// dont keep checking mirrors if they arent valid
+	return mirror
+}
+
+func isMirrorValid(mirror Mirror) Mirror {
+	if almostIdentical(mirror.Puzzle[mirror.Top], mirror.Puzzle[mirror.Bottom]) {
+		if mirror.Puzzle[mirror.Top] == mirror.Puzzle[mirror.Bottom] {
+			return mirror // keep trying mirrors that stay valid
+		}
+		// mirrors that are almost but not perfectly identical should use their wish
+		if mirror.WishUsed {
+			mirror.IsValid = false
+		} else {
+			mirror.WishUsed = true
+		}
+	}
+
+	return mirror
+}
+
+// almost identical returns true if at most one character differs between the
+// two strings
+func almostIdentical(a, b string) bool {
+	var countdiffs int
+
+	for i := 0; i < len(a); i++ {
+
+		if a[i] == b[i] {
+			continue
+		}
+
+		countdiffs++
+
+		if countdiffs > 1 {
+			return false
+		}
+	}
+
+	return true
+}
+
+// // part 2
+// func recursiveReflectionCheck(puzz []string, mirror Mirror) bool {
+// 	// fmt.Println(startBott, startTop)
+// 	if mirror.Bottom == 0 {
+// 		return almostIdentical()
+// 	}
+
+// 	if startTop == len(puzz)-1 {
+// 		return puzz[startBott] == puzz[startTop]
+// 	}
+
+// 	if puzz[startBott] != puzz[startTop] {
+// 		return false
+// 	}
+
+// 	return recursiveReflectionCheck(puzz, startBott-1, startTop+1)
+// }
 
 // didnt work
 // for _, rowId := range possibleMirrorRows {
@@ -132,22 +223,57 @@ func checkForReflection(puzzle []string) int {
 // 	}
 // }
 
-func recursiveReflectionCheck(puzz []string, startBott int, startTop int) bool {
-	// fmt.Println(startBott, startTop)
-	if startBott == 0 {
-		return puzz[startBott] == puzz[startTop]
-	}
+// part 1
+// func recursiveReflectionCheck(puzz []string, startBott int, startTop int) bool {
+// 	// fmt.Println(startBott, startTop)
+// 	if startBott == 0 {
+// 		return puzz[startBott] == puzz[startTop]
+// 	}
 
-	if startTop == len(puzz)-1 {
-		return puzz[startBott] == puzz[startTop]
-	}
+// 	if startTop == len(puzz)-1 {
+// 		return puzz[startBott] == puzz[startTop]
+// 	}
 
-	if puzz[startBott] != puzz[startTop] {
-		return false
-	}
+// 	if puzz[startBott] != puzz[startTop] {
+// 		return false
+// 	}
 
-	return recursiveReflectionCheck(puzz, startBott-1, startTop+1)
-}
+// 	return recursiveReflectionCheck(puzz, startBott-1, startTop+1)
+// }
+
+// part 2
+// used my wish indicates the wish of the smudge is true
+// func recursiveReflectionCheck(puzz []string, startBott int, startTop int, usedMyWish bool) bool {
+// 	fmt.Println(startBott, startTop)
+// 	if startBott == 0 {
+// 		if usedMyWish {
+// 			return puzz[startBott] == puzz[startTop]
+// 		} else {
+// 			// could still have a wish left
+// 			return almostIdentical(puzz[startBott], puzz[startTop])
+// 		}
+// 	}
+
+// 	if startTop == len(puzz)-1 {
+// 		if usedMyWish {
+// 			return puzz[startBott] == puzz[startTop]
+// 		} else {
+// 			// could still have a wish left
+// 			return almostIdentical(puzz[startBott], puzz[startTop])
+// 		}
+// 	}
+
+// 	if puzz[startBott] == puzz[startTop] {
+// 		recursiveReflectionCheck(puzz, startBott-1, startTop+1, usedMyWish)
+// 	}
+
+// 	if usedMyWish {
+// 		return false
+// 	} else {
+// 		// allow them to continue but no more second chances
+// 		return recursiveReflectionCheck(puzz, startBott-1, startTop+1, true)
+// 	}
+// }
 
 func flipuzzle(puzzle []string) []string {
 	out := make([]string, longestWord(puzzle))
