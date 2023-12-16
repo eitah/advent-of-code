4423e2e068c8327430b3db94cee0c99fc0990ea5
Eli Itah <eli.itah@tempus.com>
2023-12-15 23:52:12 -0600

    with small cleanup

--- 14-lens-library/main.go
+++ 14-lens-library/main.go
@@ -39,7 +39,6 @@ func main() {
 
 	var sum int
 	for _, line := range input {
-		// parts := strings.Split(line, ",")
 		parts := strings.Split(line, ",")
 		for _, word := range parts {
 			var currentVal int
@@ -55,21 +54,11 @@ func main() {
 	spew.Dump(sum)
 }
 
+// part 1
 func score(initialValue int, num rune) int {
 	out := initialValue
 	out += int(num)
-
-	fmt.Println("after add " + fmt.Sprint(out))
 	out *= 17
-	// fmt.Println("after multiply " + fmt.Sprint(out))
-
 	out = out % 256
-	fmt.Println(string(num) + " after remainder " + fmt.Sprint(out) + "\n")
-
 	return out
 }
-
-// Determine the ASCII code for the current character of the string.
-// Increase the current value by the ASCII code you just determined.
-// Set the current value to itself multiplied by 17.
-// Set the current value to the remainder of dividing itself by 256.
