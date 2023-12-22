c41eff8cac98cb907e6ad2e20d585a52937aae9a
Eli Itah <eli.itah@tempus.com>
2023-12-22 14:41:04 -0600

    Working path construction.

--- 17-clumsy-crucible/main.go
+++ 17-clumsy-crucible/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"slices"
 
-	"github.com/davecgh/go-spew/spew"
 	utils "github.com/eitah/advent-2023"
 )
 
@@ -18,26 +18,27 @@ func main() {
 	}
 
 	// Add the start to the frontier and the reached array
-	// frontier := []Hex{}
+	frontier := []Hex{}
 	reached := []Hex{}
 
-	// frontier = append(frontier, hexes[0])
+	frontier = append(frontier, hexes[0])
 	// frontier = append(frontier, hexes[0].Path(0, 0))
 	reached = append(reached, hexes[0])
 
-	hexes = addNeighbors(hexes)
+	for len(frontier) != 0 {
+		current := frontier[0]
+		for _, next := range GetNeigbors(hexes, current) {
+			if !slices.ContainsFunc(reached, hasHex(next)) {
+				frontier = append(frontier, next)
+				reached = append(reached, next)
+			}
+		}
 
-	// for len(frontier) != 0 {
-	// 	current := frontier[0]
-	// 	for _, next := range current.Neighbors {
-	// 		if !slices.ContainsFunc(reached, hasHex(next)) {
-	// 			frontier = append(frontier, next)
-	// 			reached = append(reached, next)
-	// 		}
-	// 	}
-	// }
+		frontier = frontier[1:]
+	}
 
-	spew.Dump(hexes)
+	fmt.Println("front", frontier)
+	fmt.Println("reached", reached)
 
 }
 
@@ -49,34 +50,53 @@ type Path struct {
 }
 
 type Hex struct {
-	Power     int
-	X         int
-	Y         int
-	Neighbors []Hex
+	Power int
+	X     int
+	Y     int
 }
 
-func addNeighbors(hexes []Hex) []Hex {
+// this method bleeds ineficciency because it has to find every node and look
+// for it.
+func GetNeigbors(hexes []Hex, current Hex) []Hex {
 	var out []Hex
-	for _, current := range hexes {
-		pNeighbor := []Hex{
-			{X: current.X + 1, Y: current.Y},
-			{X: current.X - 1, Y: current.Y},
-			{X: current.X, Y: current.Y + 1},
-			{X: current.X, Y: current.Y - 1},
-		}
+	pNeighbor := []Hex{
+		{X: current.X + 1, Y: current.Y},
+		{X: current.X - 1, Y: current.Y},
+		{X: current.X, Y: current.Y + 1},
+		{X: current.X, Y: current.Y - 1},
+	}
 
-		// fiind every neighboring hex and add it to the "neighbors" list
-		for _, potential := range pNeighbor {
-			if idx := slices.IndexFunc(hexes, hasHex(potential)); idx != -1 {
-				current.Neighbors = append(current.Neighbors, hexes[idx])
-			}
+	// fiind every neighboring hex and add it to the "neighbors" list
+	for _, potential := range pNeighbor {
+		if idx := slices.IndexFunc(hexes, hasHex(potential)); idx != -1 {
+			out = append(out, hexes[idx])
 		}
-
-		out = append(out, current)
 	}
 
 	return out
 }
+
+// I kept struggling with the act of getting hexes out of the array that had the element
+// func GetNeighbors(hexes []Hex) []Hex {
+// 	var out []Hex
+// 	for _, current := range hexes {
+// 		pNeighbor := []Hex{
+// 			{X: current.X + 1, Y: current.Y},
+// 			{X: current.X - 1, Y: current.Y},
+// 			{X: current.X, Y: current.Y + 1},
+// 			{X: current.X, Y: current.Y - 1},
+// 		}
+// 		// fiind every neighboring hex and add it to the "neighbors" list
+// 		for _, potential := range pNeighbor {
+// 			if idx := slices.IndexFunc(hexes, hasHex(&potential)); idx != -1 {
+// 				current.Neighbors = append(current.Neighbors, &hexes[idx])
+// 			}
+// 		}
+// 		out = append(out, current)
+// 	}
+// 	fmt.Println(out)
+// 	return out
+// }
 
 func (h *Hex) Path(sumPower, numSquares int) Path {
 	return Path{
