cdc3e9c24efdc27ed6fdd6d69ed1159af9a1dd06
Eli Itah <eli.itah@tempus.com>
2024-01-02 09:06:17 -0600

    Start A* impl

--- 17-clumsy-crucible/main.go
+++ 17-clumsy-crucible/main.go
@@ -1,28 +1,61 @@
 package main
 
 import (
+	"container/heap"
+
 	"slices"
 
 	"github.com/davecgh/go-spew/spew"
 	utils "github.com/eitah/advent-2023"
 )
 
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// we want to be able to return the lowest power item
+	return pq[i].Power < pq[j].Power
+}
+
+func (pq *PriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	hex := old[n-1]
+	old[n-1] = nil // avoid memory leak
+	hex.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return hex
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	n := len(*pq)
+	hex := x.(*Hex)
+	hex.index = n
+	*pq = append(*pq, hex)
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
 func main() {
 	input := utils.ReadFile()
 
-	var hexes []Hex
+	var hexes []*Hex
 	for posY, ln := range input {
 		for posX, char := range ln {
-			hexes = append(hexes, Hex{X: posX + 1, Y: posY + 1, Power: utils.UnsafeStringToNumber(string(char))})
+			hexes = append(hexes, &Hex{X: posX + 1, Y: posY + 1, Power: utils.UnsafeStringToNumber(string(char))})
 		}
 	}
 
 	// Add the start to the frontier and the reached array
-	frontier := []Hex{}
-	cameFrom := map[Hex]Hex{}
+	frontier := make(PriorityQueue, 0)
+	cameFrom := map[*Hex]*Hex{}
 
-	frontier = append(frontier, hexes[0])
-	// frontier = append(frontier, hexes[0].Path(0, 0))
+	heap.Init(&frontier)
+
+	heap.Push(&frontier, hexes[0])
 
 	for len(frontier) != 0 {
 		current := frontier[0]
@@ -37,39 +70,27 @@ func main() {
 		frontier = frontier[1:]
 	}
 
-	//start at the goal and work backwards to derive path
-	start := hexes[0]
-	current := hexes[len(hexes)-1]
-	path := []Hex{}
-	// until current.X and current.Y are at start
-	for current.X != start.X || current.Y != start.Y {
-		path = append(path, current)
-		current = cameFrom[current]
-	}
-
-	spew.Dump(reverse(path))
-	// fmt.Println("rev", reverse(path))
+	spew.Dump(frontier)
+	spew.Dump(cameFrom)
 
 }
 
-type Path struct {
+type PriorityQueue []*Hex
+
+type Hex struct {
+	Power               int
 	X                   int
 	Y                   int
 	NumSquaresSinceTurn int
-	SumPower            int
-}
-
-type Hex struct {
-	Power int
-	X     int
-	Y     int
+	// Priority            int
+	index int
 }
 
 // this method bleeds ineficciency because it has to find every node and look
 // for it.
-func GetNeigbors(hexes []Hex, current Hex) []Hex {
-	var out []Hex
-	pNeighbor := []Hex{
+func GetNeigbors(hexes []*Hex, current *Hex) []*Hex {
+	var out []*Hex
+	pNeighbor := []*Hex{
 		{X: current.X + 1, Y: current.Y},
 		{X: current.X - 1, Y: current.Y},
 		{X: current.X, Y: current.Y + 1},
@@ -108,26 +129,26 @@ func GetNeigbors(hexes []Hex, current Hex) []Hex {
 // 	return out
 // }
 
-func (h *Hex) Path(sumPower, numSquares int) Path {
-	return Path{
-		X:                   h.X,
-		Y:                   h.Y,
-		SumPower:            sumPower + h.Power,
-		NumSquaresSinceTurn: numSquares + 1,
-	}
-}
+// func (h *Hex) Path(sumPower, numSquares int) Path {
+// 	return Path{
+// 		X:                   h.X,
+// 		Y:                   h.Y,
+// 		SumPower:            sumPower + h.Power,
+// 		NumSquaresSinceTurn: numSquares + 1,
+// 	}
+// }
 
-func hasHex(next Hex) func(Hex) bool {
-	return func(hex Hex) bool { return next.X == hex.X && next.Y == hex.Y }
+func hasHex(next *Hex) func(*Hex) bool {
+	return func(hex *Hex) bool { return next.X == hex.X && next.Y == hex.Y }
 }
 
 // paths sometimes get generated backwards so its good to reverse to see the
 // real path
-func reverse(paths []Hex) []Hex {
-	var out []Hex
+func reverse(paths []*Hex) []*Hex {
+	var out []*Hex
 	for _, h := range paths {
 		// add new items on to the front of the array
-		out = append([]Hex{h}, out...)
+		out = append([]*Hex{h}, out...)
 	}
 	return out
 }
